3d31ce5b02f5facad4128d496f24781a2f041350
luanrem <luanrem@gmail.com>
2022-06-01 21:49:57 -0300

    insert input form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,119 @@
 import type { NextPage } from "next";
+import {
+  Box,
+  Center,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  InputRightAddon,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Stack,
+  Switch,
+  Textarea,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-  return <h1>Furniture</h1>;
+  const format = (val: string) => `R$` + val;
+  const parse = (val: string) => val.replace(/^\$/, "");
+
+  const [value, setValue] = useState<Number>(1.53);
+
+  return (
+    <Flex flexDir="column" w="100vw" h="100vh" align="center" justify="center">
+      <Box bg="gray.700" w="80%" p={4} color="white">
+        <Flex as="form" flexDir="column">
+          <Stack spacing="4">
+            <FormControl>
+              <FormLabel htmlFor="initialMoney">Valor de Entrada</FormLabel>
+              <InputGroup>
+                <NumberInput
+                  onChange={(valueString) => setValue(parse(valueString))}
+                  value={format(String(value))}
+                  max={9000}
+                >
+                  <NumberInputField />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+              </InputGroup>
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="discount">Desconto 1</FormLabel>
+              <InputGroup>
+                <Input
+                  focusBorderColor="pink.500"
+                  id="discount"
+                  name="discount"
+                  type="number"
+                  bg="gray.100"
+                  size="lg"
+                  value={500}
+                  // onChange={(event) =>
+                  //   setInputInitialMoney(event.target.value)
+                  // }
+                />
+                <InputRightAddon boxSize="mg" children="%" />
+              </InputGroup>
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="discount">Desconto 1</FormLabel>
+              <NumberInput defaultValue={15} max={30} clampValueOnBlur={false}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+            <Flex
+              flexDir="row"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <Stack direction="column">
+                <FormControl>
+                  <FormLabel htmlFor="discount">IPI</FormLabel>
+                  <NumberInput size="md" maxW={24} defaultValue={15} min={10}>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="discount">Frete</FormLabel>
+                  <NumberInput size="md" maxW={24} defaultValue={15} min={10}>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </FormControl>
+              </Stack>
+              <Flex>
+                <Stack direction="column" align="center">
+                  <FormLabel htmlFor="discount">Usar Frete + IPI</FormLabel>
+                  <Switch colorScheme="teal" size="lg" />
+                </Stack>
+              </Flex>
+            </Flex>
+          </Stack>
+        </Flex>
+      </Box>
+    </Flex>
+  );
 };
 
 export default Home;
